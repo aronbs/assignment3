194e554a749285273d48c5fe3366cc1f736ee0aa
aronbs <101139622+aronbs@users.noreply.github.com>
2022-03-08 15:10:14 +0000

    Initial commit

---
+++ bin/assignment3.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:core';
+import 'dart:math';
+import 'package:collection/collection.dart';
+
+void main(List<String> arguments){
+
+List<int> num = [1,2,4,6,6];
+
+print(num.ind(6));
+
+
+
+
+
+
+
+
+
+}
